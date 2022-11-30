85e01fe1c741e031a6648374e7113ee74f1e8a2a
Syed Mahad Shah Bukhari <110794379+mahadshahb@users.noreply.github.com>
2022-11-30 13:25:01 +0500

    wednesday-work

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,119 @@
-import React from 'react'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import currencyFormatter from 'currency-formatter';
+import { BsDash, BsPlus, BsReverseBackspaceReverse } from "react-icons/bs";
 
 const Cart = () => {
+  const { products, totalQuantities, totalPrice } = useSelector(state => state.CartReducer);
+  const dispatch = useDispatch();
+  console.log(products);
   return (
-    <div>Cart</div>
+    <React.Fragment>
+      <div className='cart'>
+        <div className='container'>
+          <h3>Your Cart</h3>
+          {
+            products.length > 0 ? <>
+              <div className='row'>
+                <div className='col-9'>
+                  <div className='cart__heading'>
+                    <div className='row'>
+                      <div className='col-2'>Picture</div>
+                      <div className='col-2'>Name</div>
+                      <div className='col-2'>Price</div>
+                      <div className='col-2'>Inc/Dec</div>
+                      <div className='col-2'>Total Price</div>
+                      <div className='col-2'>Remove</div>
+                    </div>
+                  </div>
+                  {
+                    products.map((pro) => {
+                      return (
+                        <div className='row new_vertical' key={pro.id}>
+                          <div className='col-2'>
+                            <div className='cart__image'>
+                              <img src={`/images/${pro.image}`} alt="image" />
+                            </div>
+                          </div>
+                          <div className='col-2'>
+                            <div className='cart__name'>
+                              {pro.name}
+                            </div>
+                          </div>
+                          <div className='col-2'>
+                            <div className='cart__price'>
+                              {currencyFormatter.format(pro.discountPrice, {
+                                code: "USD"
+                              })}
+                            </div>
+                          </div>
+                          <div className='col-2'>
+                            <div className='details_info'>
+                              <div className='details__incdec'>
+                                <span className='dec' onClick={() => dispatch({
+                                  type: "DEC", payload: pro.id
+                                })}><BsDash /></span>
+                                <span className='quantity'>{pro.quantity}</span>
+                                <span className='inc' onClick={() => dispatch({
+                                  type: "INC", payload:
+                                    pro.id
+                                })}><BsPlus /></span>
+                              </div>
+                            </div>
+                          </div>
+                          <div className='col-2'>
+                            <div className='cart__total'>
+                              {currencyFormatter.format(pro.discountPrice * pro.quantity, {
+                                code: "USD"
+                              })}
+                            </div>
+                          </div>
+                          <div className='col-2'>
+                            <div className='cart__remove' onClick={() => dispatch({
+                              type: 'REMOVE', payload: pro.id
+                            })}>
+                              <BsReverseBackspaceReverse />
+                            </div>
+                          </div>
+                        </div>
+                      )
+                    })
+                  }
+                </div>
+                <div className='col-3'>
+                  <div className='summary'>
+                    <div className='summary__heading'>
+                      Summary
+                    </div>
+                    <div className='summary__details'>
+                      <div className='row mb-100'>
+                        <div className='col-6'>
+                          Total Items:
+                        </div>
+                        <div className='col-6'>
+                          <div>{totalQuantities}</div>
+                        </div>
+                      </div>
+                      <div className='row'>
+                        <div className='col-6'>
+                          Total Price:
+                        </div>
+                        <div className='col-6'>
+                          <div>{currencyFormatter.format(totalPrice, {
+                            code: "USD"
+                          })}</div>
+                        </div>
+                      </div>
+                      <button type='button' className='checkout'>Checkout</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </> : "Your cart is empty"
+          }
+        </div>
+      </div>
+    </React.Fragment>
   )
 }
 
--- src/store/reducer/CartReducer.js
+++ src/store/reducer/CartReducer.js
@@ -1,16 +1,18 @@
 const initialState = {
-    produts: [],
+    products: [],
     totalPrice: 0,
     totalQuantities: 0,
 }
 
 const CartReducer = (state = initialState, action) => {
+    let findPro;
+    let index;
     switch (action.type) {
         case 'ADD_TO_CART':
             // console.log(action.payload.product.id);
             const { product, quantity } = action.payload;
             // console.log(product.id, quantity);
-            const check = state.produts.find(pr => pr.id === product.id);
+            const check = state.products.find(pr => pr.id === product.id);
             if (check) {
                 return state;
             }
@@ -20,9 +22,40 @@ const CartReducer = (state = initialState, action) => {
                 const Tquantities = state.totalQuantities + quantity;
                 product.quantity = quantity;
                 return {
-                    ...state, produts: [...state.produts, product], totalPrice: Tprice,
+                    ...state, products: [...state.products, product], totalPrice: Tprice,
                     totalQuantities: Tquantities
                 }
+            }
+        case "INC":
+            findPro = state.products.find(product => product.id === action.payload);
+            index = state.products.findIndex(product => product.id === action.payload);
+            findPro.quantity += 1;
+            state.products[index] = findPro;
+            return {
+                ...state,
+                totalPrice: state.totalPrice + findPro.discountPrice, totalQuantities: state.totalQuantities + 1
+            }
+        case "DEC":
+            findPro = state.products.find(product => product.id === action.payload);
+            index = state.products.findIndex(product => product.id === action.payload);
+            if (findPro.quantity > 1) {
+                findPro.quantity -= 1;
+                state.products[index] = findPro;
+                return {
+                    ...state,
+                    totalPrice: state.totalPrice - findPro.discountPrice, totalQuantities: state.totalQuantities - 1
+                }
+            }
+            else {
+                return state;
+            }
+        case "REMOVE":
+            findPro = state.products.find(product => product.id === action.payload);
+            const filtered = state.products.filter(product => product.id !== action.payload);
+            return {
+                ...state,
+                products: filtered,
+                totalPrice: state.totalPrice - findPro.discountPrice * findPro.quantity, totalQuantities: state.totalQuantities - findPro.quantity
             }
         default: return state
     }
